3aff2cd0dc543dc891b01c7897fe3f938aed1029
Chia Hsin <chiahsin@Chias-MacBook-Air.local>
2014-12-29 12:29:01 -0800

    delete create folder

--- www/app/main/drinkMenu/drinkMenu.js
+++ www/app/main/drinkMenu/drinkMenu.js
@@ -63,23 +63,4 @@ angular.module('app.main.drinkMenu', [
 	    // Execute action
 	  });
 
-      $scope.closeModal = function() {
-        $scope.modal.hide();
-      };
-
-    // Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.modal.remove();
-    });
-
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-      // Execute action
-    });
-
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-    });
-
   });
